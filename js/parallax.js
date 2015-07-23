ad4ef07c6404d8d9969c596d421a9e7bb21132f2
tuskerette <valeria.graffeo@gmail.com>
2015-07-23 15:33:20 -0400

    added animations and ground image

--- js/parallax.js
+++ js/parallax.js
@@ -1,5 +1,15 @@
 $(document).ready(function() {
 
+  $('.bigjello').on('mouseenter', function(){
+    $('.bigjello').addClass('animated jello infinite');
+  });
+  $(window).scroll(function(){
+    $('.umbrella').addClass('animated swing');
+  });
+  $('.umbrella').on('mouseenter', function(){
+    $('.umbrella').toggleClass('animated swing');
+  });
+
   /* Scroll event handler */
     $(window).bind('scroll',function(e){
       parallaxScroll();
@@ -11,7 +21,7 @@ function parallaxScroll(){
   var scrolled = $(window).scrollTop();
   $('#parallax-small-raindrops').css('top',(0-(scrolled*.16))+'px');
   $('#parallax-big-raindrops').css('top',(0-(scrolled*.26))+'px');
-  $('#parallax-big-jello').css('top',(0-(scrolled*.4))+'px');
-  $('#parallax-umbrella').css('top',(0-(scrolled*.07))+'px');
+  $('#parallax-big-jello').css('top',(0-(scrolled*.43))+'px');
+  $('#parallax-umbrella').css('top',(0-(scrolled*.01))+'px');
 }
 
--- readme.md
+++ readme.md
@@ -3,8 +3,9 @@ Big Jello has an umbrella!
 
 ### This is my first experiment with parallax.
 
-You can see it live here: [link](http://www.tuskerette.com/projects/BigJelloParallax/)
+You can see it live on tuskerette.com: [here](http://www.tuskerette.com/projects/BigJelloParallax/)
 
-I used the tutorial by Jonathan Nicol
-[link](http://jonathannicol.com/blog/2011/08/06/build-a-parallax-scrolling-website-interface-with-jquery-and-css/)
+I used the tutorial by [Jonathan Nicol](http://jonathannicol.com/blog/2011/08/06/build-a-parallax-scrolling-website-interface-with-jquery-and-css/)
+
+Animate CSS by [Daniel Eden](http://daneden.me/animate)
 
